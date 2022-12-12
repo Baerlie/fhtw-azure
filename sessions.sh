5c23b073af059e4605d468227dd821ebcf11f8bc
Beatrice Pscheidl <beatrice.pscheidl@gmail.com>
2022-12-12 09:18:03 +0100

    streamlit port 80

--- sessions.sh
+++ sessions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tmux new -d -s lab3 \; split-window -h ;\ 
-tmux send-keys -t lab3.0 "streamlit run dashboard.py" ENTER
+tmux send-keys -t lab3.0 "streamlit run dashboard.py --server.port=80" ENTER
 tmux send-keys -t lab3.1 "uvicorn src.main:app" ENTER
 
 tmux a -t lab3
