fa16abcab5dd9d45fcf40d66a96fd6977793d42c
Beatrice Pscheidl <beatrice.pscheidl@gmail.com>
2022-12-16 12:05:12 +0100

    Update sessions.sh

--- sessions.sh
+++ sessions.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-export TERM=xterm
-tmux new -d -s lab3 \; split-window -h ;\ 
-tmux send-keys -t lab3.0 "streamlit run dashboard.py --server.port=80" ENTER
-tmux send-keys -t lab3.1 "uvicorn src.main:app" ENTER
-
-tmux a -t lab3
+streamlit run dashboard.py --server.port=80 &>/dev/null &
+uvicorn src.main:app &>/dev/null &
